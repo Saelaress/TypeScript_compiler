d4ec1e442b095f7bffe0aca62c7dd7b19b87977b
Saelaress <Nadejdakna2002@gmail.com>
2023-10-22 11:05:06 +0300

    Исправлены ошибки в h файле и добавлены статические функции

--- tree_nodes.h
+++ tree_nodes.h
@@ -1,17 +1,30 @@
-enum exprType{
-    int_type, id_type, plust_type, assiyn_type, array_type, func_typr 
-}
-class expr_node{
-    public: 
+#include <string>
+#include <vector>
+
+enum exprType
+{
+    int_type,
+    id_type,
+    plus_type,
+    assign_type,
+    array_type,
+    func_type
+};
+
+class expr_node
+{
+public:
     exprType type;
     int value;
-    string strID;
+    int Id_node;
+    std::string strID;
     expr_node *left;
     expr_node *right;
-    expr_list_node *expr_list;
-    int Id_node;
-    std::vector <expr_node*> *params;
-    static expr_node *createExprFromBinPlus(exprType typeOpr, expr_node *leftOpr, expr_node *rightOpr);
+    std::vector<expr_node *> *params;
 
-
-}
+    static expr_node *create_expr_from_int(int val);
+    static expr_node *create_expr_from_id(std::string &id);
+    static expr_node *create_expr_from_bin_op(exprType typeOpr, expr_node *leftOpr, expr_node *rightOpr);
+    static expr_node *create_expr_from_fun(std::string &id, std::vector<expr_node *> *params_list);
+    static int max_id;
+};
